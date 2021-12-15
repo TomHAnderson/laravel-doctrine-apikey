d09e43b5055334bfc6e5dfdf743fe1e24ee36657
Tom H Anderson <tom.h.anderson@gmail.com>
2021-12-15 03:03:52 -0700

    Added service provider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ApiSkeletons\Laravel\Doctrine\ApiKey;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+class ServiceProvider extends LaravelServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                Command\CreateApiKey::class,
+                Command\UpdateApiKey::class,
+                Command\DeleteApiKey::class,
+                Command\CreateScope::class,
+                Command\DeleteScope::class,
+                Command\AddScopeToApiKey::class,
+                Command\RemoveScopeFromApiKey::class,
+            ]);
+        }
+    }
+}
